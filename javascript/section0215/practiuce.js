5260d7d70d0724d3299b623d37caa7d54b4ce578
yoonji1492 <yoonji1492@gmail.com>
2024-02-15 17:28:32 +0900

    반복문 commit

---
+++ javascript/section0215/practiuce.js
@@ -0,0 +1,43 @@
+let array = [{object1}, {}]
+
+object : 객체
+
+바구니 :
+여러가지 정보
+기능 단위
+
+상품 이미지 + 상품명 + 가격 -> object
+
+
+
+json : 데이터 덩어리, 객체 모아놓은 파일, key + value
+
+let arr = [
+    {
+        id:'0',
+        img : 'url',
+        title : '제목',
+        price : '12000'
+    },
+    {
+        id:'1',
+        img : 'url',
+        title : 'sdfsdf',
+        price : '345436'
+    },
+    {
+        id:'2',
+        데이터 베이스의 인덱스
+        img : 'url',
+        title : 'dfgdfg',
+        price : '324542'
+    }
+
+]
+
+arr[0].price
+
+URLSearchParams()
+
+파라미터 값을 주는 함수 
+상품명 - 정렬 
